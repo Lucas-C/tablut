8eba20617db5b017ebc26f9dc0427965498ac238
Lucas Cimon <lucas.cimon@gmail.com>
2018-06-04 21:20:17 +0200

    Adding game variant in gameoptions.inc.php

--- gameoptions.inc.php
+++ gameoptions.inc.php
@@ -22,28 +22,13 @@
 
 $game_options = array(
 
-    /* Example of game variant:
-
-
     // note: game variant ID should start at 100 (ie: 100, 101, 102, ...). The maximum is 199.
     100 => array(
-                'name' => totranslate('my game option'),
+                'name' => totranslate('King in the corners'),
                 'values' => array(
-
-                            // A simple value for this option:
-                            1 => array( 'name' => totranslate('option 1') )
-
-                            // A simple value for this option.
-                            // If this value is chosen, the value of "tmdisplay" is displayed in the game lobby
-                            2 => array( 'name' => totranslate('option 2'), 'tmdisplay' => totranslate('option 2') ),
-
-                            // Another value, with other options:
-                            //  beta=true => this option is in beta version right now.
-                            //  nobeginner=true  =>  this option is not recommended for beginners
-                            3 => array( 'name' => totranslate('option 3'),  'beta' => true, 'nobeginner' => true ),) )
+                            1 => array( 'name' => totranslate('On') )
+                            2 => array( 'name' => totranslate('Off' ),
                         )
             )
-
-    */
 
 );
